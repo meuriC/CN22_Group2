7ffe1e8ac8b3101b2d0a7a2e8c1ffccf7ccbe4e8
Pedro Santos <fc46417@alunos.fc.ul.pt>
2022-03-28 22:51:07 +0100

    first scripts

---
+++ scripts/make.sh
@@ -0,0 +1,11 @@
+ #!/bin/sh
+
+########################################################
+
+## Shell Script to Build Docker Image 
+
+########################################################
+
+echo "Setting up the API"
+#Chamar Docker Compose
+#O shell poe no docker desktop o docker file
---
+++ scripts/populate_databases.sh
@@ -0,0 +1,22 @@
+ #!/bin/sh
+
+########################################################
+
+## Shell Script to Populate databases
+
+########################################################
+
+
+printf "Populating Databases\n"
+printf "\t Populating Games"
+# { python3 "./api/databases/games/PopulateGames.py" } &> /dev/null
+printf "done\n"
+
+printf "\t Populating Reviews"
+# { python3 "./api/databases/reviews/PopulateReviews.py"
+#  python3 "./api/databases/reviews/PopulateReviews2.py" } &> /dev/null
+printf "done\n"
+
+printf "\t Populating Users"
+# { python3 "./api/databases/reviews/PopulateUsers.py" &> /dev/null
+printf "done\n" 
