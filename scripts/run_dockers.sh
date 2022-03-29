5e38e7fe5e62b50437ee44d4ccfd43c15910acc1
Pedro Santos <fc46417@alunos.fc.ul.pt>
2022-03-30 00:06:35 +0100

    run_dockers runs games and reviews. Missing users

--- scripts/run_dockers.sh
+++ scripts/run_dockers.sh
@@ -1,7 +1,7 @@
  printf "Building Docker Network first\n"
 
-{ sudo docker network create microservices
-} &> /dev/null
+##{ sudo docker network create microservices
+#} &> /dev/null
 
 
 #printf "\t Running Games"
@@ -10,9 +10,13 @@
 
 
 printf "\t Running Users"
-sudo docker run -p 127.0.0.1:50052:50052/tcp games --network="microservices" --name "users" &
+sudo docker run -p 127.0.0.1:50052:50052/tcp users --network="microservices" --name "users" &
 printf "done\n"
 
 printf "\t Running Reviews"
-sudo  docker run -p 127.0.0.1:50051:50051/tcp reviews --network="microservices" --name "reviews" 
+sudo  docker run -p 127.0.0.1:50051:50051/tcp reviews --network="microservices" --name "reviews" & 
 printf "done\n"
+
+printf "\t Running Games"
+sudo docker run -p 127.0.0.1:50052:50052/tcp games --network="microservices" --name "games" 
+printf "done\n
