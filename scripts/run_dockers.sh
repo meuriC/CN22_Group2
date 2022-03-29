baa5ced7e8003b5e2e275ef9949b7d85b648cc25
Pedro Santos <fc46417@alunos.fc.ul.pt>
2022-03-29 20:40:18 +0100

    docker run outfix before I leave

--- scripts/run_dockers.sh
+++ scripts/run_dockers.sh
@@ -4,10 +4,15 @@
 } &> /dev/null
 
 
-printf "\t Running Games"
-sudo docker run -p 127.0.0.1:50051:50051/tcp games --network="microservices" --name "games" &
+#printf "\t Running Games"
+#sudo docker run -p 127.0.0.1:50051:50051/tcp games --network="microservices" --name "games" &
+#printf "done\n"
+
+
+printf "\t Running Users"
+sudo docker run -p 127.0.0.1:50052:50052/tcp games --network="microservices" --name "users" &
 printf "done\n"
 
-printf "\t Running Games"
-sudo docker run -p 127.0.0.1:50055:50055/tcp account --network="microservices" --name "games" &
+printf "\t Running Reviews"
+sudo  docker run -p 127.0.0.1:50051:50051/tcp reviews --network="microservices" --name "reviews" 
 printf "done\n"
