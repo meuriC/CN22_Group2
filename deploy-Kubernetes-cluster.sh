cf2e98d517e36963705d40c20890bddea12dc967
meuriC <94645356+meuriC@users.noreply.github.com>
2022-04-22 00:03:47 +0100

    small fix

--- deploy-Kubernetes-cluster.sh
+++ deploy-Kubernetes-cluster.sh
@@ -1,20 +1,20 @@
-echo "Creating Kubernetes Cluster"
+echo "--Creating Kubernetes Cluster--"
 {
 	gcloud container clusters create cluster-steam --zone europe-west1-b --cluster-version 1.22.8-gke.200 --release-channel rapid --num-nodes=1
 }
-echo "Conecting to  Kubernetes Cluster"
+echo "--Connecting to  Kubernetes Cluster--"
 {
 	gcloud container clusters get-credentials cluster-steam --zone europe-west1-b
 }
-echo "Moving to Deployment folder"
+echo "--Moving to Deployment folder--"
 {
-	cd /CN22_Group2/deployment
+	cd /deployment
 }
-echo "Deploying Services"
+echo "--Deploying Services--"
 {
 	kubectl apply -f services-deploy.yaml 
 }
-echo "Deploying Ingress"
+echo "--Deploying Ingress--"
 {
 	kubectl apply -f ingress-deploy.yaml 
 }
