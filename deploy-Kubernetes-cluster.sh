53f8df7a1ccaf305e04fa588d58e21e8d5c66e49
meuriC <94645356+meuriC@users.noreply.github.com>
2022-04-21 23:43:42 +0100

    Script for cloud automatic deploy

---
+++ deploy-Kubernetes-cluster.sh
@@ -0,0 +1,20 @@
+echo "Creating Kubernetes Cluster"
+{
+	gcloud container clusters create cluster-steam --zone europe-west1-b --cluster-version 1.22.8-gke.200 --release-channel rapid --num-nodes=1
+}
+echo "Conecting to  Kubernetes Cluster"
+{
+	gcloud container clusters get-credentials cluster-steam --zone europe-west1-b
+}
+echo "Moving to Deployment folder"
+{
+	cd /CN22_Group2/deployment
+}
+echo "Deploying Services"
+{
+	kubectl apply -f services-deploy.yaml 
+}
+echo "Deploying Ingress"
+{
+	kubectl apply -f ingress-deploy.yaml 
+}
