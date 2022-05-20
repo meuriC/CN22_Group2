cbf452013071e91d9df3b3f51d57990b47cb4af7
Miguel Silva <miguelescsilva@gmail.com>
2022-05-20 20:04:51 +0100

    Auth0 working, error SSL_ERROR_RX_RECORD_TOO_LONG pelo tamanho do url de redirect que vem com o token de acesso

--- api/microservices/gateway/api-gateway.py
+++ api/microservices/gateway/api-gateway.py
@@ -1,21 +1,65 @@
 import connexion
 from yaml import Loader, load
 from flask_swagger_ui import get_swaggerui_blueprint
+from flask_cors import CORS
+from authlib.integrations.flask_client import OAuth
+import json
+from flask import jsonify
+from flask import redirect
 
 # create the application instance
-app = connexion.App(__name__, specification_dir="./")
-swagger_yml = load(open("swagger.yaml", 'r'), Loader = Loader)
+options = {
+    "swagger_ui_config": {
+        "oauth2RedirectUrl": "https://localhost:5000/ui/oauth2-redirect.html",
+    }
+}
+app = connexion.App(__name__, specification_dir="./", options=options)
+swagger_yml = load(open("swagger.yaml", 'r'), Loader=Loader)
 
-swaggerui_blueprint = get_swaggerui_blueprint("/ui", "swagger.yaml", config = {'spec': swagger_yml})
+swaggerui_blueprint = get_swaggerui_blueprint(
+    "/ui",
+    "swagger.yaml",
+    config={
+        'spec': swagger_yml, 'oauth2RedirectUrl': "https://localhost:5000/ui/oauth2-redirect.html"},
+    oauth_config={
+        'clientId': "sUnKWfmlaywQm64EtEZpioz5uRK5GAzc"}
+)
 app.app.register_blueprint(swaggerui_blueprint)
+
+CORS(app.app)
+app.app.config['SESSION_TYPE'] = 'memcached'
+app.app.config['SECRET_KEY'] = 'secretKey safe'
+oauth = OAuth(app.app)
+auth0 = oauth.register(
+    'auth0',
+    client_id='sUnKWfmlaywQm64EtEZpioz5uRK5GAzc',
+    client_secret='AbC4iHv7TlkNvKXpxEv88dRBmcYu6MykpyN8SBsIKIC7G5pOegcySMEgTDW7vDLd',
+    api_base_url='https://dev-p3dnwrxe.us.auth0.com',
+    access_token_url='https://dev-p3dnwrxe.us.auth0.com/oauth/token',
+    authorize_url='https://dev-p3dnwrxe.us.auth0.com/authorize',
+    client_kwargs={
+        'scope': 'open',
+    },
+)
 
 # swagger.yml file to configure the endpoints
 app.add_api("swagger.yaml")
+
+
+@app.app.route('/login')
+def call():
+    return auth0.authorize_redirect(redirect_uri="https://localhost:5000/ui/callback",  audience='https://cn22group2/')
+
+
+@app.app.route('/callback')
+def callback_handling():
+    return jsonify(auth0.authorize_access_token())
+
 
 @app.app.route('/health')
 def health():
     return "OK", 200
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-	
--- api/microservices/gateway/api-users.py
+++ api/microservices/gateway/api-users.py
@@ -1,32 +1,119 @@
 import os
 import re
 
+
+import json
+from six.moves.urllib.request import urlopen
+
+from jose import jwt
+
+from flask import Flask, jsonify, request, url_for, abort, g
+from flask import redirect
+from six.moves.urllib.parse import urlencode
+
 import grpc
 from grpc_interceptor import ExceptionToStatusInterceptor
 
 from users_pb2_grpc import UsersStub
 from users_pb2 import *
 
+AUTH0_DOMAIN = 'dev-p3dnwrxe.us.auth0.com'
+API_AUDIENCE = 'https://cn22group2/'
+ALGORITHMS = ["RS256"]
+
 users_host = os.getenv("USERS_HOST", "localhost")
 users_channel = grpc.insecure_channel(f"{users_host}:50052")
 users_client = UsersStub(users_channel)
+jsonurl = urlopen("https://"+AUTH0_DOMAIN+"/.well-known/jwks.json")
+jwks = json.loads(jsonurl.read())
+
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+
+def handle_auth_error(ex):
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
+
+
+def verify_token(access_token) -> dict:
+
+    unverified_header = jwt.get_unverified_header(access_token)
+    rsa_key = {}
+    for key in jwks["keys"]:
+        if key["kid"] == unverified_header["kid"]:
+            rsa_key = {
+                "kty": key["kty"],
+                "kid": key["kid"],
+                "use": key["use"],
+                "n": key["n"],
+                "e": key["e"]
+            }
+    if rsa_key:
+        try:
+            payload = jwt.decode(
+                access_token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer="https://"+AUTH0_DOMAIN+"/"
+            )
+        except jwt.ExpiredSignatureError:
+            handle_auth_error(AuthError({"code": "token_expired",
+                                         "description": "token is expired"}, 401))
+        except jwt.JWTClaimsError:
+            handle_auth_error(AuthError({"code": "invalid_claims",
+                                         "description":
+                                         "incorrect claims,"
+                                         "please check the audience and issuer"}, 401))
+        except Exception:
+            handle_auth_error(AuthError({"code": "invalid_header",
+                                         "description":
+                                         "Unable to parse authentication"
+                                         " token."}, 401))
+    unverified_claims = jwt.get_unverified_claims(access_token)
+    if unverified_claims.get("scope") and unverified_claims.get("sub"):
+        token_scopes = unverified_claims["scope"].split()
+        return {'sub': unverified_claims.get("sub"), 'scope': token_scopes}
+    handle_auth_error(AuthError({"code": "invalid_header",
+                                 "description":
+                                     "Unable to parse authentication"
+                                     " token."}, 401))
+
+
+def logoutUser():
+    return redirect('https://dev-p3dnwrxe.us.auth0.com/v2/logout')
+
+
+def loginUser():
+    s = request.url_root + "login"
+    return redirect(s)
 
 
 def createUser(UserItem):
-    request = CreateUserRequest(user_language = UserItem["language"], user_name = UserItem["username"])  
+    request = CreateUserRequest(
+        user_language=UserItem["language"], user_name=UserItem["username"])
     response = users_client.CreateUser(request)
     return {"user_name": response.user_name, "user_id": response.user_id}
-	
+
+
 def getUser(user_id):
-    request = IdRequest(user_id = user_id)
+    request = IdRequest(user_id=user_id)
     response = users_client.GetUserById(request)
     return {"user_name": response.user_name, "user_id": response.user_id, "user_language": response.user_language, "user_num_reviews": response.user_num_reviews, "user_num_games_owned": response.user_num_games_owned}
-	
+
+
 def deleteUserAccount(user_id):
-    request = IdRequest(user_id = user_id)
-    return users_client.DeleteUserById(request).deleted 
+    request = IdRequest(user_id=user_id)
+    return users_client.DeleteUserById(request).deleted
+
 
 def createReview(user_id, app_id, ReviewsItem):
-    request = CreateReviewRequest(app_id = app_id, review = ReviewsItem["review"], recommended = ReviewsItem["recommended"], user_id = user_id)
+    request = CreateReviewRequest(
+        app_id=app_id, review=ReviewsItem["review"], recommended=ReviewsItem["recommended"], user_id=user_id)
     response = users_client.PostReview(request)
     return {"user_id": response.author_steam_id, "app_id": response.app_id, "review": response.review, "recommended": response.recommended}
