4de30cfc6bed267263a3a7cccf328e20d1c740e8
meuriC <94645356+meuriC@users.noreply.github.com>
2022-05-22 17:39:12 +0100

    Games tests with pytest

---
+++ api/microservices/games/tests/test_gameMicroservice.py
@@ -0,0 +1,19 @@
+import sys
+sys.path.append('../')
+
+from games_pb2 import *
+from games_pb2_grpc import GamesStub
+import grpc
+
+channel = grpc.insecure_channel("localhost:50051")
+client = GamesStub(channel)
+gameName = "Noddy"
+def test_createGame():
+    request = CreateGameRequest(name = gameName)
+    response = client.CreateGame(request)
+    assert response.name == gameName, " test createGame failed"
+
+def test_getGameByName ():
+    request = GameByNameRequest(name= gameName)
+    response = client.GameByName(request)
+    assert response.name == gameName, " test gameByName failed"
