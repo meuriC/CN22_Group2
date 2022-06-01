84dbf4c79011190be1a7a919798a42a9d34b7597
meuriC <94645356+meuriC@users.noreply.github.com>
2022-06-01 01:03:39 +0100

    Microservice UnitTests

--- api/microservices/games/tests/test_gameMicroservice.py
+++ api/microservices/games/tests/test_gameMicroservice.py
@@ -18,9 +18,23 @@ def test_getGameByName ():
     response = client.GameByName(request)
     assert response.name == gameName, " test gameByName failed"
 
-'''
+
 def test_getRecommendedGames():
     request = GetMostRecommendedGamesRequest(max_results=5)
     response = client.GetRecommendedGames(request)
-    assert len(response) == 5, " test getRecommendedGames failed"
-'''
+    assert len(response.game) == 5, " test getRecommendedGames failed"
+
+def test_deleteGame():
+    request = GameByNameRequest(name = "Noddy")
+    response = client.DeleteGameByName(request)
+    assert response.deleted == True, "test_deleteGame failed"
+
+def test_GetMostReviewedGames():
+    request = GetMostReviewedGamesRequest(max_results=5)
+    response = client.GetGames(request)
+    assert len(response.game) == 5, " test_GetMostReviewedGames failed"
+
+def test_gameReviews():
+    request = GameReviewsRequest(app_id="883710", max_results=2)
+    response = client.GameReviews(request)
+    assert len(response.game) == 5, " test_gameReviews failed"
---
+++ api/microservices/reviews/test_scripts/test_reviewsMicroservice.py
@@ -0,0 +1,41 @@
+import sys
+sys.path.append('../')
+
+from reviews_pb2 import *
+from reviews_pb2_grpc  import ReviewsStub
+import grpc
+
+channel = grpc.insecure_channel("localhost:50053")
+client = ReviewsStub(channel)
+gameName = "Noddy"
+def test_updateReview():
+    request = UpdateReviewByIdRequest(review_id="test", review="This is a test review by yours truly ren XD", recommended="True")
+    response = client.PutReview(request)
+    assert response.review=="This is a test review by yours truly ren XD", " test test_updateReview failed"
+
+def test_reviewById():
+    request = ReviewByIdRequest(review_id="50463082")  #50463082
+    response = client.GetReview(request)
+    assert response.review_id=="50463082"
+
+def test_getgameReviews():
+    request = ReviewsByGameRequest(app_id="883710", max_results=5)
+    response =  client.GetGameReviews(request)
+    assert len(response) == 5, "test_getgameReviews failded "
+
+
+def test_putHelpfulReview():
+    request = UpdateHelpfulOnReviewByIdRequest(review_id="test", votes_helpful="1")
+    response = client.PutHelpfulReview(request)
+    assert response.votes_helpful== 1, "test_PutHelpfulReview failed"
+
+def test_deleteReview():
+    request = ReviewByIdRequest(review_id="test")
+    response = client.DeleteReview(request)
+    assert response.status== True, "test_DeleteReview failed"
+
+
+def test_getReviewsByUser():
+    request = ReviewsByUserIdRequest(author_steam_id="76561198054155096", max_results=5)
+    response = client.GetReviewsByUser(request)
+    assert len(response) == 5, "test_getReviewsByUser failed"
