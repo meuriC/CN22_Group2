a119094bed1d39a25226a7e77573e0b9292f0334
renribeiro <91562371+renribeiro@users.noreply.github.com>
2022-06-09 12:24:57 +0100

    Update deploy-Kubernetes-cluster.sh

--- scripts/deploy-Kubernetes-cluster.sh
+++ scripts/deploy-Kubernetes-cluster.sh
@@ -1,5 +1,3 @@
-#git clone https://github.com/meuriC/CN22_Group2.git
-
 echo "--Creating Kubernetes Cluster--" 
 gcloud container clusters create cluster-steam --zone europe-west1-b --cluster-version 1.21.12-gke.1700 --release-channel rapid --num-nodes=3
 
@@ -9,7 +7,7 @@ echo "--Connecting to Kubernetes Cluster--"
 gcloud container clusters get-credentials cluster-steam --zone europe-west1-b
 
 echo "--Moving to Deployment folder--" 
-cd deployment
+cd ../deployment
 
 echo "--Deploying Services--" 
 kubectl apply -f services-deploy.yaml
